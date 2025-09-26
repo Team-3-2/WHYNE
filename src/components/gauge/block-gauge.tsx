31f1cc14530f221420f5e4fc5d9a8a41b8fa4505
junye0l <junyeolkim00@gmail.com>
2025-09-26 09:48:38 +0900

    refactor : 컴포넌트에 'use client' 지시문 추가

--- src/components/gauge/block-gauge.tsx
+++ src/components/gauge/block-gauge.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 
 type GaugeLevel = 0 | 1 | 2 | 3 | 4 | 5 | 6;
--- src/components/taste/Taste.tsx
+++ src/components/taste/Taste.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import BlockGauge, { type GaugeLevel } from "../gauge/block-gauge";
 
