5e7a523c09e65bcb1e5b023dc06eda194007ff17
Yeonsu Lee <170384198+suuuuya@users.noreply.github.com>
2025-09-27 05:31:54 +0900

    feat: 아이콘 버튼 컴포넌트 추가 #9

--- src/components/button/IconButton.tsx
+++ src/components/button/IconButton.tsx
@@ -1,3 +1,30 @@
-const IconButton = () => {};
+import { cn } from "@/lib/utils";
+import Icon from "../icon/Icon";
+import {
+  COMMON_BUTTON_STYLES,
+  BUTTON_SHAPE_VARIANTS,
+  BUTTON_STATE_VARIANTS,
+} from "./style";
+
+interface IconButtonProps {
+  icon: keyof typeof import("../icon/icon-map").default;
+  className?: string;
+}
+
+const IconButton = ({ icon, className, ...props }: IconButtonProps) => {
+  return (
+    <button
+      className={cn(
+        COMMON_BUTTON_STYLES.base,
+        BUTTON_SHAPE_VARIANTS.round,
+        BUTTON_STATE_VARIANTS.secondary,
+        className
+      )}
+      {...props}
+    >
+      <Icon icon={icon} />
+    </button>
+  );
+};
 
 export default IconButton;
