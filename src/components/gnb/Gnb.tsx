759df4a80289df02331a2f46cc62bbcff3a6f7e5
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-27 01:19:54 +0900

    design: 내 프로필 페이지 임시 커밋

--- src/components/gnb/Gnb.tsx
+++ src/components/gnb/Gnb.tsx
@@ -19,7 +19,7 @@ const Gnb = () => {
   return (
     <header
       className={cn(
-        "fixed left-1/2 top-0 flex -translate-x-1/2 items-center justify-between bg-black",
+        "fixed left-1/2 top-0 z-50 flex -translate-x-1/2 items-center justify-between bg-black",
         "h-[50px] w-full gap-[10px] px-5 py-[15px]",
         "tablet:rounded-1 tablet:top-10 tablet:h-[70px] tablet:w-[704px] tablet:px-[60px] tablet:py-[11px]",
         "pc:rounded-1 pc:top-10 pc:h-[70px] pc:w-[1140px] pc:px-[60px] pc:py-[11px]"
