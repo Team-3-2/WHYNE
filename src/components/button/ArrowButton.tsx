f848065faa22673a27e5b81cb025f3924b4c2d4a
Yeonsu Lee <170384198+suuuuya@users.noreply.github.com>
2025-09-27 05:20:49 +0900

    feat: 화살표 이동 버튼 컴포넌트 추가 #9

--- src/components/button/ArrowButton.tsx
+++ src/components/button/ArrowButton.tsx
@@ -1,3 +1,32 @@
-const ArrowButton = () => {};
+import { cn } from "@/lib/utils";
+import Icon from "../icon/Icon";
+import {
+  COMMON_BUTTON_STYLES,
+  BUTTON_SHAPE_VARIANTS,
+  BUTTON_STATE_VARIANTS,
+} from "./style";
+
+interface ArrowButtonProps {
+  direction: "left" | "right";
+  className?: string;
+}
+
+const ArrowButton = ({ direction, className, ...props }: ArrowButtonProps) => {
+  const iconName = direction === "left" ? "ArrowLeftIcon" : "ArrowRightIcon";
+  return (
+    <button
+      aria-label={direction === "left" ? "이전으로 이동" : "다음으로 이동"}
+      className={cn(
+        COMMON_BUTTON_STYLES.base,
+        BUTTON_SHAPE_VARIANTS.round,
+        BUTTON_STATE_VARIANTS.secondary,
+        className
+      )}
+      {...props}
+    >
+      <Icon icon={iconName} />
+    </button>
+  );
+};
 
 export default ArrowButton;
