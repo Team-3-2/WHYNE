4c1d2430a8bcfa217e206879b6c726c422f71ba6
junye0l <junyeolkim00@gmail.com>
2025-09-26 18:57:17 +0900

    design : add review-taste component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,6 @@ export { default as Header } from "./header/Header";
 export { TextInput, ModalTextInput } from "./text-input/text-input";
 export { default as SelectType } from "./select-type/select-type";
 export { default as Icon } from "./icon/Icon";
+export { default as BlockGauge } from "./gauge/block-gauge";
+export { default as DetailTaste } from "./taste/detail-taste";
+export { default as ReviewTaste } from "./taste/review-taste";
