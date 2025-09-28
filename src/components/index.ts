f5564f3050afb48225bd47b42146267acad83a0a
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-28 15:42:46 +0900

    rename: header fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as Gnb } from "./gnb/Gnb";
-export { default as Header } from "./header/Header";
+export { default as Header } from "./header/header";
 export { default as DropdownMenu } from "./dropdown-menu/DropdownMenu";
 export { default as Chip } from "./chip/Chip";
 export { default as Flavor } from "./flavor/Flavor";
