75bd78f3f4819feb5bd782290eea567548b04d23
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-28 15:49:51 +0900

    rename: dropdown-menu fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { default as Gnb } from "./gnb/gnb";
 export { default as Header } from "./header/header";
-export { default as DropdownMenu } from "./dropdown-menu/DropdownMenu";
+export { default as DropdownMenu } from "./dropdown-menu/dropdown-menu";
 export { default as Chip } from "./chip/Chip";
 export { default as Flavor } from "./flavor/flavor";
 export { default as TextInput } from "./text-input/text-input";
