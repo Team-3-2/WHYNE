d4ccc3c3b2790fce91afa867500bf2fcd0744921
junye0l <junyeolkim00@gmail.com>
2025-09-29 03:31:54 +0900

    design : 랜딩페이지 디자인 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,33 @@
 import Image from "next/image";
+import Button from "@/components/button/basic-button";
 import { cn } from "@/lib/utils";
 
 const Home = () => {
   return (
     <>
-      <div className="flex w-full justify-center overflow-hidden">
+      {/* TODO(김준열): 메인 배너의 포함되는 텍스트의 위치가 조금 어색함 PR 피드백 이후 수정*/}
+      <div className="relative flex w-full justify-center overflow-hidden">
         <Image
           src="/images/landing/landing-banner.jpg"
           alt="와인 추천 서비스 메인 배너"
           width={1920}
-          height={664}
-          sizes="1920px"
+          height={730}
           priority
-          style={{ minWidth: "1920px", height: "664px" }}
+          style={{ minWidth: "1920px" }}
         />
+        <div className="absolute inset-0 ml-12 flex items-start justify-start pt-24 tablet:ml-24 pc:ml-80 pc:pt-48">
+          <div className="text-white">
+            <h1
+              className={cn(
+                "flex flex-col leading-tight",
+                "text-title-page-sm tablet:text-title-page-sm pc:text-title-page-sm"
+              )}
+            >
+              <span>한 곳에서 관리하는</span>
+              <span>나만의 와인창고</span>
+            </h1>
+          </div>
+        </div>
       </div>
 
       <main
@@ -113,10 +127,9 @@ const Home = () => {
         </section>
       </main>
 
-      <footer className="flex-center py-8">
-        <button className="rounded-lg bg-blue-500 px-6 py-3 text-white transition-colors hover:bg-blue-600">
-          임시 버튼
-        </button>
+      {/* TODO(김준열): 와인 보러가기 버튼 클릭시 /wines로 이동하게끔 추후에 연결 */}
+      <footer className="flex-center w-full px-16 py-16">
+        <Button label="와인 보러가기" className="h-[50px] w-[283px]" />
       </footer>
     </>
   );
