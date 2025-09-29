95deba6b9e255cb01c221c5d29c0371e17161216
Yeonsu Lee <170384198+suuuuya@users.noreply.github.com>
2025-09-29 17:03:13 +0900

    fix: 배열 방식 수정 #16

--- src/components/star-rating/rating-breakdown.tsx
+++ src/components/star-rating/rating-breakdown.tsx
@@ -37,7 +37,7 @@ const RatingBreakdown = ({
             "pc:gap-y-[8px]"
           )}
         >
-          {[...Array(maxRating)].map((_, i) => {
+          {Array.from({ length: maxRating }).map((_, i) => {
             const score = maxRating - i;
             const count = distribution[score] ?? 0;
             return (
