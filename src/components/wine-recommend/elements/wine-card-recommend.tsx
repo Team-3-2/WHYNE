72e1d37a4678c64ef9380ab1598cfa188578d747
JungHoon <fbwjdgns05@naver.com>
2025-10-01 16:04:44 +0900

    feat: 리스트 페이지네이션 일단 완료

--- src/components/wine-recommend/elements/wine-card-recommend.tsx
+++ src/components/wine-recommend/elements/wine-card-recommend.tsx
@@ -7,43 +7,38 @@ type RecommendWineProps = {
   image: string;
 };
 
-const card_text_design =
-  "tracking-[-0.02em] pt-[6px] tablet:pt-2 pc:pt-2  tablet:max-h-[48px] pc:max-h-[48px]";
-
 const RecommendWineCard = ({ id, name, region, image }: RecommendWineProps) => {
   return (
     <div
       className={cn(
-        "flex h-[243px] w-[150px] flex-col items-center",
+        "relative flex h-[243px] w-[150px] flex-col items-center",
         "tablet:h-[320px] tablet:w-[201px]",
         "pc:h-[320px] pc:w-[201px]"
       )}
     >
       <img
         className={cn(
-          "mx-auto h-[165px] w-auto object-contain pb-[6px]",
-          "tablet:h-[228px] tablet:pb-2",
-          "pc:h-[228px] pc:pb-2"
+          "mx-auto h-[165px] w-auto object-contain",
+          "tablet:h-[228px]",
+          "pc:h-[228px]"
         )}
         src={image}
         alt={name}
       />
       <h2
         className={cn(
-          "line-clamp-2 max-h-10 text-center text-sm text-gray-900",
-          "tablet:max-h-12 tablet:text-body-md",
-          "pc:max-h-12 pc:text-body-md",
-          card_text_design
+          "line-clamp-2 pt-[12px] text-center text-sm tracking-[-0.02em] text-gray-900",
+          "tablet:h-16 tablet:pt-4 tablet:text-body-md",
+          "pc:h-16 pc:pt-4 pc:text-body-md"
         )}
       >
         {name}
       </h2>
       <p
         className={cn(
-          "line-clamp-1 max-h-10 text-center text-[12px] leading-5 text-gray-500",
-          "tablet:text-sm",
-          "pc:text-sm",
-          card_text_design
+          "bottom-0 line-clamp-1 pt-[6px] text-center text-[12px] leading-5 tracking-[-0.02em] text-gray-500",
+          "tablet:pt-2 tablet:text-sm",
+          "pc:pt-2 pc:text-sm"
         )}
       >
         {region}
--- src/components/wine-recommend/wine-recommend.tsx
+++ src/components/wine-recommend/wine-recommend.tsx
@@ -1,9 +1,15 @@
 import { recommendwinemock } from "@/mock";
 import ArrowButton from "@/components/button/arrow-button";
 import { cn } from "@/lib/utils";
+import RecommendWineCard from "./elements/wine-card-recommend";
+import { useState } from "react";
+
 const data = recommendwinemock;
 
 const WineRecommend = () => {
+  const [index, setIndex] = useState<number>(0);
+  const pages = 4;
+  const pagedata = data.slice(index, index + pages);
   return (
     <div
       className={cn(
@@ -12,49 +18,57 @@ const WineRecommend = () => {
         "pc:h-[603px] pc:rounded-b-[88px]"
       )}
     >
-      <h2
-        className={cn(
-          "left-[37px] top-[85px] text-[30px] font-bold text-gray-800",
-          "tablet:left-20 tablet:top-[127px]",
-          "pc:left-[88px] pc:top-[154px]"
-        )}
-      >
-        이번달 추천 와인
-      </h2>
-      <div>
-        <ArrowButton
-          direction="prev"
-          className="z-10 hidden h-12 w-12 tablet:left-16 tablet:block pc:left-0 pc:block"
-        />
-        {/*disabled={isLoading}
-          onClick={() => setIndex(index - 1)} */}
-        <div
+      <div className="relative flex w-full flex-col gap-[19px] tablet:gap-[33px] tablet:pb-11 pc:mx-auto pc:w-[1195px] pc:gap-[39px] pc:pb-[60px]">
+        <h2
           className={cn(
-            "flex flex-row gap-8 mobile:overflow-x-auto",
-            "tablet:grid tablet:max-h-[320px] tablet:grid-cols-3 tablet:grid-rows-1 tablet:gap-1 tablet:overflow-hidden",
-            "pc:grid pc:grid-cols-4 pc:grid-rows-1 pc:gap-[85px]"
+            "pl-[37px] pt-[85px] text-heading-md text-gray-800",
+            "tablet:pl-20 tablet:pt-[127px]",
+            "pc:pl-[88px] pc:pt-[154px]"
           )}
         >
-          {/*        
-            {data?.map(({ id, name, region, image }) => (
-            <RecommendWineCard
-            key={id}
-            id={id}
-            name={name}
-            region={region}
-            image={image}
-            />
+          이번 달 추천 와인
+        </h2>
+        <div
+          className={cn(
+            "relative flex w-full flex-row tablet:h-[320px] pc:h-[320px]"
+          )}
+        >
+          <ArrowButton
+            direction="prev"
+            className="relative z-10 h-12 w-12 mobile:hidden tablet:left-16 tablet:top-[114px] tablet:block pc:left-0 pc:top-[136px] pc:block"
+            onClick={() => setIndex(index - 1)}
+            disabled={index == 0}
+          />
+          {/*
+           */}
+          <div
+            className={cn(
+              "mx-auto flex flex-row items-center gap-8 mobile:overflow-x-auto",
+              "tablet:h-[320px] tablet:gap-1",
+              "pc:gap-[85px]"
+            )}
+          >
+            {pagedata?.map(({ id, name, region, image }) => (
+              <RecommendWineCard
+                key={id}
+                id={id}
+                name={name}
+                region={region}
+                image={image}
+              />
             ))}
+          </div>
+          <ArrowButton
+            direction="next"
+            className="relative z-10 h-12 w-12 mobile:hidden tablet:right-16 tablet:top-[114px] tablet:block pc:right-0 pc:top-[136px] pc:block"
+            onClick={() => setIndex(index + 1)}
+            disabled={index + pages == 8}
+          />
+          {/*        
+          
+          
         */}
         </div>
-        <ArrowButton
-          direction="next"
-          className="z-10 hidden h-12 w-12 tablet:right-16 tablet:top-[114px] tablet:block pc:right-0 pc:top-[136px] pc:block"
-        />
-        {/*        
-          disabled={isLoading}
-          onClick={() => setIndex(index + 1)}
-        */}
       </div>
     </div>
   );
