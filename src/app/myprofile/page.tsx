f761d6a27c29503b37dbf8a0520a3373d3711533
wlrnjs <tjwlrnjs7336@icloud.com>
2025-10-04 22:11:12 +0900

    design: 임시 로더 추가

--- src/app/myprofile/page.tsx
+++ src/app/myprofile/page.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react";
 import { MyProfile } from "./_components";
+import Loader from "@/components/loader/Loader";
 
 const Page = () => {
   return (
     // TODO(지권): 로딩 컴포넌트 추가
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loader />}>
       <MyProfile />
     </Suspense>
   );
---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+
+export default function Loader() {
+  const bgColor = "bg-black/60";
+  const innerColor = "rgb(139, 0, 0)";
+
+  return (
+    <div
+      className={cn(
+        "flex h-screen w-screen items-center justify-center",
+        bgColor
+      )}
+    >
+      <div className="relative">
+        <span
+          className="absolute left-1/2 top-1/2 text-[38px] tracking-[5px] text-white"
+          style={{
+            transform: "translate(-50%, -50%)",
+            color: "transparent",
+            WebkitTextStroke: `0.3px ${innerColor}`,
+          }}
+        >
+          whyne
+        </span>
+        <span
+          className="absolute left-1/2 top-1/2 text-[38px] tracking-[5px]"
+          style={{
+            transform: "translate(-50%, -50%)",
+            color: innerColor,
+            WebkitTextStroke: `1px ${innerColor}`,
+            animation: "whyne723 2s ease-in-out infinite",
+          }}
+        >
+          whyne
+        </span>
+      </div>
+
+      {/* keyframes 정의 */}
+      <style jsx>{`
+        @keyframes whyne723 {
+          0%,
+          100% {
+            clip-path: polygon(
+              0% 45%,
+              15% 44%,
+              32% 50%,
+              54% 60%,
+              70% 61%,
+              84% 59%,
+              100% 52%,
+              100% 100%,
+              0% 100%
+            );
+          }
+          50% {
+            clip-path: polygon(
+              0% 60%,
+              16% 65%,
+              34% 66%,
+              51% 62%,
+              67% 50%,
+              84% 45%,
+              100% 46%,
+              100% 100%,
+              0% 100%
+            );
+          }
+        }
+      `}</style>
+    </div>
+  );
+}
