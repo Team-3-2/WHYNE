b76830f6e1f8500642fa4d2688339d3174fce6da
wlrnjs <tjwlrnjs7336@icloud.com>
2025-10-09 03:14:39 +0900

    feat: 주석 추가

--- src/app/myprofile/_components/my-profile/my-profile.tsx
+++ src/app/myprofile/_components/my-profile/my-profile.tsx
@@ -1,30 +1,20 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import AccountItem from "../account-item/account-item";
 import ReviewItem from "../review-item/review-item";
 import ProfileTabs from "../profile-tabs/profile-tabs";
-import instance from "@/lib/axios";
-import { User } from "@/types/user-type";
+import useUserStore from "@/store/user-store";
+
+// TODO(지권): 이슈 발생...
 
 const MyProfile = () => {
   const searchParams = useSearchParams();
   const [tab, setTab] = useState(searchParams.get("tab") || "review");
-  const [user, setUser] = useState<User>();
+  const { user } = useUserStore((state) => state);
 
-  const getMe = async () => {
-    try {
-      const response = await instance.get("/users/me");
-      setUser(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getMe();
-  }, []);
+  if (!user) return redirect("/login");
 
   return (
     <main className="flex-col-center mx-auto w-full pc:flex-row pc:items-start">
