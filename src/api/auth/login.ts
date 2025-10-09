e52a2103dce039feadd82d542ffea3804ecfbf7e
Imhwitae <ghkdgnlxo2@gmail.com>
2025-10-09 11:12:49 +0900

    feature: 로그인 API

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -1,5 +1,8 @@
+"use server";
+
 import instance from "@/lib/axios";
 import { User } from "@/types/user-type";
+import { cookies } from "next/headers";
 
 interface LoginData {
   user: User;
@@ -7,13 +10,46 @@ interface LoginData {
   refreshToken: string;
 }
 
-const login = async (data: any): Promise<LoginData | undefined> => {
+// const login = async (data: any): Promise<LoginData | undefined> => {
+//   try {
+//     const response = await instance.post("/auth/signIn", data);
+
+//     if (!response) throw new Error("데이터를 불러오는데 실패했습니다.");
+
+//     return response.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+const login = async (prevState: any, formData: FormData) => {
+  console.log(formData);
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  if (!(email || password))
+    return { isError: true, message: "이메일또는 비밀번호를 입력하세요." };
+
   try {
-    const response = await instance.post("/auth/signIn", data);
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/signIn`,
+      {
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+      }
+    );
 
-    if (!response) throw new Error("데이터를 불러오는데 실패했습니다.");
+    if (!response.ok)
+      return { isError: true, message: "이메일 혹은 비밀번호를 확인해주세요." };
+    // sessionStorage.setItem("accessToken", response?.accessToken);
+    //   localStorage.setItem("refreshToken", response?.refreshToken);
+    const cookieStore = await cookies();
+    cookieStore.set("name", "test");
 
-    return response.data;
+    const userInfo = await response.json();
+
+    return { isError: false, data: userInfo?.user };
   } catch (error) {
     console.error(error);
   }
