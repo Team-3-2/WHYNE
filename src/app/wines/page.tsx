cb9e6af2ace1bfa1d1b2b34f4d808a3e48c4ca6e
wlrnjs <tjwlrnjs7336@icloud.com>
2025-10-10 21:30:48 +0900

    design: 반응형 임시 구현

--- src/app/wines/page.tsx
+++ src/app/wines/page.tsx
@@ -1,6 +1,10 @@
 import { Suspense } from "react";
 import WineRecommended from "@/app/wines/_components/wine-recommended/wine-recommended";
 import Loader from "@/components/loader/loader";
+import WineSearchOption from "@/app/wines/_components/wine-search-option/wine-search-option";
+import WineList from "@/app/wines/_components/wine-list/wine-list";
+import { cn } from "@/lib/utils";
+import { Searchbar } from "@/components";
 
 /**
  * 와인 목록 페이지
@@ -10,6 +14,23 @@ const WineListPage = () => {
   return (
     <Suspense fallback={<Loader />}>
       <WineRecommended />
+      <section
+        aria-labelledby="browse-title"
+        className={cn(
+          "container flex w-full flex-col gap-[24px]",
+          "tablet:gap-[40px]",
+          "pc:mt-[45px] pc:flex-row pc:items-start pc:gap-[60px]"
+        )}
+      >
+        {/* 검색 옵션 */}
+        <WineSearchOption />
+
+        {/* 검색바 */}
+        <Searchbar className="order-1 flex-1 pc:order-2" />
+
+        {/* 와인 목록 */}
+        <WineList />
+      </section>
     </Suspense>
   );
 };
