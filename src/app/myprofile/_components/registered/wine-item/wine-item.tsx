0a25cdb332f1b38bfc8d67965ffb558092d3e44b
Yeonsu Lee <170384198+suuuuya@users.noreply.github.com>
2025-10-12 23:15:12 +0900

    feat: 삭제하기 컨펌 모달 적용 #109

--- src/app/myprofile/_components/registered/wine-item/wine-item.tsx
+++ src/app/myprofile/_components/registered/wine-item/wine-item.tsx
@@ -1,26 +1,44 @@
 "use client";
 
+import { useState } from "react";
 import { Card } from "@/components";
 import useDeleteWine from "@/hooks/api/wines/use-delete-wine";
-import { WineType } from "../../../_types/review-type";
+import { ConfirmModal } from "@/components";
+import { WineType } from "@/app/myprofile/_types/review-type";
 
 const WineItem = ({ wine }: { wine: WineType }) => {
   const { mutate } = useDeleteWine();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const handleWineDelete = () => {
     mutate({ id: wine.id });
+    setIsModalOpen(false);
   };
 
   return (
-    <Card
-      image={wine.image}
-      name={wine.name}
-      region={wine.region}
-      price={wine.price}
-      actionMenuItems={[
-        { label: "수정하기", onClick: () => {} },
-        { label: "삭제하기", onClick: handleWineDelete },
-      ]}
-    />
+    <>
+      <Card
+        image={wine.image}
+        name={wine.name}
+        region={wine.region}
+        price={wine.price}
+        actionMenuItems={[
+          { label: "수정하기", onClick: () => {} },
+          { label: "삭제하기", onClick: () => setIsModalOpen(true) },
+        ]}
+      />
+      <ConfirmModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onConfirm={handleWineDelete}
+        className="max-w-[353px]"
+        msg={{
+          text: `${wine.name} 와인을\n 정말 삭제할까요?`,
+          cancelMsg: "취소",
+          confirmMsg: "삭제하기",
+        }}
+      />
+    </>
   );
 };
 
