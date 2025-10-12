c6f5f01ded464925a7f97a7eaa9afaf4b1199548
wlrnjs <wlrnjs5824@gmail.com>
2025-10-13 04:47:26 +0900

    feat: 와인 목록 모달 구현

--- src/app/wines/_components/wine-options/type-option.tsx
+++ src/app/wines/_components/wine-options/type-option.tsx
@@ -75,7 +75,7 @@ const TypeOption = () => {
   return (
     <div className="flex flex-col gap-5">
       <h2 className="text-body-lg">타입</h2>
-      <div className="flex flex-col gap-2">
+      <div className="flex gap-2 pc:flex-col">
         {(WINE_TYPE as WineType[]).map((item) => (
           <TypeItem
             key={item}
--- src/app/wines/_components/wine-search-option/wine-search-option.tsx
+++ src/app/wines/_components/wine-search-option/wine-search-option.tsx
@@ -2,11 +2,77 @@
 
 import { Button, Icon } from "@/components";
 import { useBreakpoint } from "@/hooks/use-breakpoint";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
+import { cn, allowScroll, lockingScroll } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import { PriceOption, RatingOption, TypeOption } from "../wine-options";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+interface ModalProps {
+  open: number;
+  setModalOpen: (open: number) => void;
+  resetSignal: number;
+  resetFn: () => void;
+}
+
+const Modal = ({ open, setModalOpen, resetSignal, resetFn }: ModalProps) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  useEffect(() => {
+    if (!dialogRef.current?.open && open) {
+      dialogRef.current?.showModal();
+      lockingScroll();
+    } else {
+      dialogRef.current?.close();
+    }
+
+    return () => {
+      allowScroll();
+    };
+  }, [open]);
+
+  if (!open) return null;
+
+  return (
+    <dialog
+      ref={dialogRef}
+      className="z-50 border-0 bg-transparent p-0 [&::backdrop]:bg-black/50"
+      onClick={() => setModalOpen(0)}
+    >
+      <div
+        className="mx-auto my-0 flex w-[375px] flex-col items-start overflow-hidden rounded-[16px] bg-white p-[24px] shadow-lg"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="flex flex-col gap-y-[32px]">
+          <div className="flex w-full items-center justify-between">
+            <h2 className="text-heading-md">필터</h2>
+            <button onClick={() => setModalOpen(0)} aria-label="검색 필터 닫기">
+              <Icon icon="XIcon" size="lg" />
+            </button>
+          </div>
+          <div className="flex flex-col gap-[16px]">
+            <TypeOption />
+            <hr />
+            <PriceOption resetSignal={resetSignal} />
+            <hr />
+            <RatingOption />
+          </div>
+        </div>
+        <div className="mt-10 flex w-full items-center justify-between gap-[8px]">
+          <Button
+            className="h-[54px] w-[1/3]"
+            variant="outline"
+            onClick={resetFn}
+            label="초기화"
+          />
+          <Button
+            className="h-[54px] flex-1"
+            onClick={() => setModalOpen(0)}
+            label="필터 적용하기"
+          />
+        </div>
+      </div>
+    </dialog>
+  );
+};
 
 const WineSearchOption = ({
   setSearch,
@@ -15,6 +81,7 @@ const WineSearchOption = ({
 }) => {
   const router = useRouter();
   const breakpoint = useBreakpoint();
+  const [modalOpen, setModalOpen] = useState(0);
   const [resetSignal, setResetSignal] = useState(0);
   const isMobileOrTablet = breakpoint === "mobile" || breakpoint === "tablet";
 
@@ -28,16 +95,22 @@ const WineSearchOption = ({
     <>
       {isMobileOrTablet ? (
         <div className="order-2 flex justify-between">
-          <Link
-            href="/"
+          <button
             aria-label="옵션 열기"
             className="flex-center h-[42px] w-[42px] rounded-[8px] border border-gray-300 bg-white tablet:h-12 tablet:w-12"
+            onClick={() => setModalOpen((prev) => prev + 1)}
           >
             <Icon icon="FilterIcon" className="text-secondary" />
-          </Link>
+          </button>
           <Button
             label="와인 등록하기"
             className="w-[160px] tablet:w-[220px]"
+          />
+          <Modal
+            open={modalOpen}
+            setModalOpen={setModalOpen}
+            resetFn={handleResetClick}
+            resetSignal={resetSignal}
           />
         </div>
       ) : (
