4eb274b7339b726f8a878a80dacb7c68a4e224a1
wlrnjs <wlrnjs5824@gmail.com>
2025-10-14 00:43:41 +0900

    fix: 크로매틱 배포 에러 수정

--- src/components/select-type/select-type.stories.tsx
+++ src/components/select-type/select-type.stories.tsx
@@ -1,6 +1,15 @@
 import { Meta, StoryObj } from "@storybook/nextjs";
 import SelectType from "./select-type";
 
+const registerMock = (): any => {
+  return (name: string) => ({
+    name,
+    onChange: () => {},
+    onBlur: () => {},
+    ref: () => {},
+  });
+};
+
 const meta: Meta<typeof SelectType> = {
   title: "공통 컴포넌트/SelectType",
   component: SelectType,
@@ -15,11 +24,15 @@ export default meta;
 type Story = StoryObj<typeof SelectType>;
 
 export const WineTypeSelect: Story = {
-  args: {},
+  args: {
+    register: registerMock(),
+    isError: false,
+  },
 };
 
 export const WineTypeSelectError: Story = {
   args: {
+    register: registerMock(),
     isError: true,
   },
 };
