29bfc4d7b0788e34510d479ced1f279476388a6b
wlrnjs <wlrnjs5824@gmail.com>
2025-10-13 19:14:29 +0900

    fix: GNB .container 추가

--- src/components/gnb/gnb.tsx
+++ src/components/gnb/gnb.tsx
@@ -41,52 +41,54 @@ const Gnb = ({ user }: { user: User }) => {
       <header
         className={cn(
           "fixed top-0 z-[9999] flex items-center justify-between bg-[#171A21]",
-          "h-[60px] w-full gap-[10px] px-5 py-[15px]",
-          "tablet:h-[70px] tablet:px-[30px] tablet:py-[11px]",
-          "pc:h-[70px] pc:px-[300px] pc:py-[11px]"
+          "h-[60px] w-full gap-[10px] py-[15px]",
+          "tablet:h-[70px] tablet:py-[11px]",
+          "pc:h-[70px] pc:py-[11px]"
         )}
       >
-        <Link
-          href="/"
-          aria-label="메인페이지 이동"
-          className="flex items-center leading-[26px] text-white"
-        >
-          <Logo className="w-[100px]" />
-        </Link>
+        <div className="container flex items-center justify-between">
+          <Link
+            href="/"
+            aria-label="메인페이지 이동"
+            className="flex items-center leading-[26px] text-white"
+          >
+            <Logo className="w-[100px]" />
+          </Link>
 
-        <div className="relative">
-          {user ? (
-            <button
-              type="button"
-              aria-label="메뉴 열기"
-              onClick={() => setIsOpen((prev) => !prev)}
-              className="flex items-center"
-            >
-              <Image
-                src={user?.image || "/images/profile/default-profile.svg"}
-                alt="프로필 이미지"
-                width={45}
-                height={45}
-                className="h-[45px] w-[45px] rounded-full"
-              />
-            </button>
-          ) : (
-            <Link
-              href="/login"
-              className="leading-[26px] text-white underline-offset-2 hover:underline"
-            >
-              로그인
-            </Link>
-          )}
+          <div className="relative">
+            {user ? (
+              <button
+                type="button"
+                aria-label="메뉴 열기"
+                onClick={() => setIsOpen((prev) => !prev)}
+                className="flex items-center"
+              >
+                <Image
+                  src={user?.image || "/images/profile/default-profile.svg"}
+                  alt="프로필 이미지"
+                  width={45}
+                  height={45}
+                  className="h-[45px] w-[45px] rounded-full"
+                />
+              </button>
+            ) : (
+              <Link
+                href="/login"
+                className="leading-[26px] text-white underline-offset-2 hover:underline"
+              >
+                로그인
+              </Link>
+            )}
 
-          {isOpen && (
-            <div className="absolute right-0 z-[60] mt-2">
-              <DropdownMenu
-                items={dropDownMenuItems}
-                itemClassName="hover:text-black"
-              />
-            </div>
-          )}
+            {isOpen && (
+              <div className="absolute right-0 z-[60] mt-2">
+                <DropdownMenu
+                  items={dropDownMenuItems}
+                  itemClassName="hover:text-black"
+                />
+              </div>
+            )}
+          </div>
         </div>
       </header>
     </>
