5c5b57ec07a6e9bcc26393a9664d62064da7ff5c
wlrnjs <wlrnjs5824@gmail.com>
2025-10-14 21:37:31 +0900

    chore: 최신화 커밋

--- src/app/(auth)/_components/remember-id.tsx
+++ src/app/(auth)/_components/remember-id.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+
 interface RememberIdProps {
   checked: boolean;
   setChecked: (checked: boolean) => void;
@@ -6,25 +8,29 @@ interface RememberIdProps {
 function RememberId({ checked, setChecked }: RememberIdProps) {
   return (
     <div className="space-y-2">
-      <label
-        htmlFor="remember_id"
-        className="inline-flex cursor-pointer select-none items-center gap-2"
-      >
-        <input
-          id="remember_id"
-          name="remember_id"
-          type="checkbox"
-          checked={checked}
-          onChange={(e) => setChecked(e.target.checked)}
-          aria-describedby="remember_id_help"
-          className="h-5 w-5 rounded border-gray-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-600 focus-visible:ring-offset-2"
-        />
-        <span>아이디 저장</span>
-      </label>
-
       <span className="sr-only" aria-live="polite">
         아이디 저장 {checked ? "켜짐" : "꺼짐"}
       </span>
+      <div className="flex items-center gap-[14px] pc:gap-3">
+        <input
+          type="checkbox"
+          name="remember_id"
+          id="remember_id"
+          className={cn(
+            "relative h-5 w-5 appearance-none rounded-[4px] border border-gray-300 bg-white transition-colors",
+            "checked:border-tertiary",
+            "after:absolute after:inset-[3px] after:scale-0 after:rounded-[2px] after:bg-black after:transition-transform checked:after:scale-100"
+          )}
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+        <label
+          className="cursor-pointer select-none text-body-sm text-default pc:text-body-md"
+          htmlFor="remember_id"
+        >
+          아이디 저장
+        </label>
+      </div>
     </div>
   );
 }
