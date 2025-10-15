c3822301e34f1a35d8d44146fff6e339327fd05e
Yeonsu Lee <170384198+suuuuya@users.noreply.github.com>
2025-10-15 16:42:35 +0900

    chore: 와인 상세 페이지 메타데이터 작업 중단 #149

--- src/app/wines/[id]/page.tsx
+++ src/app/wines/[id]/page.tsx
@@ -1,50 +1,58 @@
 import { Suspense } from "react";
-import type { Metadata, ResolvingMetadata } from "next";
+import instance from "@/lib/axios";
 import { METADATA } from "@/constants/metadata";
+import type { Metadata, ResolvingMetadata } from "next";
 import { cookies } from "next/headers";
 import WineDetailContent from "@/app/wines/[id]/_components/wine-detail/wine-detail-content";
 import Loader from "@/components/loader/loader";
 
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
+// type Props = {
+//   params: { id: string };
+//   searchParams: { [key: string]: string | string[] | undefined };
+// };
 
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const { id } = await params;
-  const cookieStore = await cookies();
-  const cookieString = cookieStore.toString();
-  const wine = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/wines/${id}`
-  ).then((res) => res.json());
-  const PAGE_TITLE = `${wine.name ?? ""} 와인 상세 정보`;
-  const DESCRIPTION = "와인 상세 정보 페이지입니다.";
+// export async function fetchWineDetail(id: string) {
+//   const cookieHeader = cookies().toString();
+//   const res = await instance.get(`/wines/${id}`, {
+//     method: "GET",
+//     headers: {
+//       Cookie: cookieHeader,
+//     },
+//   });
+//   return res.data;
+// }
 
-  return {
-    ...METADATA,
-    title: PAGE_TITLE,
-    description: DESCRIPTION,
-    openGraph: {
-      ...METADATA.openGraph,
-      title: PAGE_TITLE,
-      description: DESCRIPTION,
-    },
-    twitter: {
-      ...METADATA.twitter,
-      title: PAGE_TITLE,
-      description: DESCRIPTION,
-    },
-  };
-}
+// export async function generateMetadata(
+//   { params, searchParams }: Props,
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//   const wine = await fetchWineDetail(params.id);
+
+//   const PAGE_TITLE = `${wine.name} 와인 상세 정보`;
+//   const DESCRIPTION = "와인 상세 정보 페이지입니다.";
+
+//   return {
+//     ...METADATA,
+//     title: PAGE_TITLE,
+//     description: DESCRIPTION,
+//     openGraph: {
+//       ...METADATA.openGraph,
+//       title: PAGE_TITLE,
+//       description: DESCRIPTION,
+//     },
+//     twitter: {
+//       ...METADATA.twitter,
+//       title: PAGE_TITLE,
+//       description: DESCRIPTION,
+//     },
+//   };
+// }
 
 const WineDetailPage = async ({ params }: { params: { id: string } }) => {
-  const { id } = await params;
+  //const wine = await fetchWineDetail(params.id);
   return (
     <Suspense fallback={<Loader />}>
-      <WineDetailContent wineId={Number(id)} />
+      <WineDetailContent wineId={Number(params.id)} />
     </Suspense>
   );
 };
