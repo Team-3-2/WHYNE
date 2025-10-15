dc25ce2bfb8b0156fd915723ca6662e02081fda4
Yeonsu Lee <170384198+suuuuya@users.noreply.github.com>
2025-10-15 22:26:31 +0900

    chore: 사이트맵에서 마이페이지 코드 줄 삭제 #149

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -16,12 +16,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.9,
     },
     {
-      url: `${SITE_URL}/myprofile`,
-      lastModified: new Date().toISOString(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
       url: `${SITE_URL}/login`,
       lastModified: new Date().toISOString(),
       changeFrequency: "never",
