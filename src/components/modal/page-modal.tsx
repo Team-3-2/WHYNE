325b36c90bac78e3612230e7f44db47b0ae30a03
Imhwitae <ghkdgnlxo2@gmail.com>
2025-10-16 21:47:30 +0900

    fix: 페이지 모달 모바일 화면 수정

--- src/components/modal/page-modal.tsx
+++ src/components/modal/page-modal.tsx
@@ -55,7 +55,7 @@ const PageModal = ({
     <Modal
       ref={modalRef}
       className={cn(
-        "border-none px-6 mobile:mt-[94px] tablet:relative pc:relative",
+        "h-[738px] border-none px-6 tablet:relative pc:relative",
         "mobile:bottom-0 mobile:left-0 mobile:right-0 mobile:mb-0 mobile:w-full mobile:max-w-full",
         "mobile:rounded-none mobile:rounded-t-2xl",
         "tablet:h-[1010px]",
