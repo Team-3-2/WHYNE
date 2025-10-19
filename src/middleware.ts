362996cbe7505da4c2a0f1c5bb491fa4b547dc79
Imhwitae <ghkdgnlxo2@gmail.com>
2025-10-19 21:23:17 +0900

    feat: 로그인 이전 페이지로 리다이렉트 기능 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 
 const middleware = (request: NextRequest) => {
   const token = request.cookies.get("accessToken")?.value;
+  const requestedPage = request.nextUrl.pathname;
 
-  if (token) {
+  if (!token) {
+    if (requestedPage === "/login" || requestedPage === "/signup") {
+      return NextResponse.next();
+    }
+
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    url.searchParams.set("redirect", requestedPage);
+
+    return NextResponse.redirect(url);
+  }
+
+  if (requestedPage === "/login" || requestedPage === "/signup") {
     return NextResponse.redirect(new URL("/myprofile", request.url));
   }
 
@@ -11,7 +24,14 @@ const middleware = (request: NextRequest) => {
 };
 
 export const config = {
-  matcher: ["/login", "/signup"],
+  matcher: [
+    "/myprofile",
+    "/wine/:path*",
+    "/register/:path*",
+    "/review/:path*",
+    "/login",
+    "/signup",
+  ],
 };
 
 export default middleware;
